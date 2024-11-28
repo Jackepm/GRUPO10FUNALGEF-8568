10d6df1a8a951bb714bb9725337602c51fed362b
avenaconpalta <willtbtxd@gmail.com>
2024-11-28 07:30:51 -0500

    PRIMER COMMIT clienteutil

---
+++ AlquilerCarros/ClienteUtil.cs
@@ -0,0 +1,25 @@
+﻿public class ClienteUtil
+{
+    public int Dni { get; set; }
+    public string Nombre { get; set; }
+    public double PagoMaximo { get; set; }
+
+    public ClienteUtil(double pagoMaximo)
+    {
+        PagoMaximo = pagoMaximo;
+    }
+
+    public Cliente(int dni, string nombre, double pagoMaximo)
+    {
+        Dni = dni;
+        Nombre = nombre;
+        PagoMaximo = pagoMaximo;
+    }
+
+    public override string ToString()
+    {
+        return $"DNI: {Dni}, Nombre: {Nombre}, Pago Máximo: {PagoMaximo:C}";
+    }
+}
+
+
